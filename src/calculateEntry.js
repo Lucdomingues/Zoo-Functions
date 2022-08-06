0ee193c49f2e19f222df4a37535af49e2cb0158c
Lucdomingues <lojalucc@outlook.com>
2022-08-06 17:19:50 -0300

    requisito 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const verificaFaixa = entrants.reduce((acc, curr) => acc, {});
+  const verificaFaixa = { adult: 0, child: 0, senior: 0 };
+  entrants.reduce((acc, curr) => {
+    if (curr.age < 18) {
+      verificaFaixa.child += 1;
+    } else if (curr.age >= 18 && curr.age < 50) {
+      verificaFaixa.adult += 1;
+    } else if (curr.age >= 50) {
+      verificaFaixa.senior += 1;
+    }
+    return verificaFaixa;
+  }, '');
   return verificaFaixa;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const obj = countEntrants(entrants);
+  const multAdult = obj.adult * data.prices.adult;
+  const multChild = obj.child * data.prices.child;
+  const multSenior = obj.senior * data.prices.senior;
+  const total = multAdult + multChild + multSenior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const semParametro = data.employees.map((element) => {
+  const la = element.responsibleFor.reduce((acc, curr) => `${acc}, ${curr}`);
+  const obje = {
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: data.species.filter((i) => i.id.includes(la)),
+  };
+  return obje;
+});
 
+function getEmployeesCoverage(obj) {
+  return console.log(semParametro);
+}
+getEmployeesCoverage();
 module.exports = getEmployeesCoverage;
