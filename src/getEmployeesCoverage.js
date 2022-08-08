65f77d03dbaddbd32f3d002044c063ec4f3cfece
Lucdomingues <lojalucc@outlook.com>
2022-08-08 20:40:15 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,17 +1,46 @@
 const data = require('../data/zoo_data');
-
-const semParametro = data.employees.map((element) => {
-  const la = element.responsibleFor.reduce((acc, curr) => `${acc}, ${curr}`);
+//-------------------------------------------------------------------------------------
+const semParametro = () => data.employees.map((element) => {
   const obje = {
     id: element.id,
     fullName: `${element.firstName} ${element.lastName}`,
-    species: data.species.filter((i) => i.id.includes(la)),
+    species: element.responsibleFor.map((animalId) => data.species
+      .find((comp) => comp.id.includes(animalId)).name),
+    locations: element.responsibleFor.map((animalId) => data.species
+      .find((comp) => comp.id.includes(animalId)).location),
   };
   return obje;
 });
-
+//-------------------------------------------------------------------------------------
+let busca;
+//--------------------------------------------------------------------------------------
+const comParametro = () => {
+  const objeParam = {
+    id: busca.id,
+    fullName: `${busca.firstName} ${busca.lastName}`,
+    species: busca.responsibleFor.map((animalId) => data.species
+      .find((comp) => comp.id.includes(animalId)).name),
+    locations: busca.responsibleFor.map((animalId) => data.species
+      .find((comp) => comp.id.includes(animalId)).location),
+  };
+  return objeParam;
+};
+//-------------------------------------------------------------------------------------
 function getEmployeesCoverage(obj) {
-  return console.log(semParametro);
+  if (!obj) {
+    return semParametro();
+  }
+  if (obj.name) {
+    busca = data.employees.find((nome) => nome.firstName === obj.name
+      || nome.lastName === obj.name);
+  }
+  if (obj.id) {
+    busca = data.employees.find((elementId) => elementId.id === obj.id);
+  }
+  if (!busca) {
+    throw new Error('Informações inválidas');
+  }
+  return comParametro();// função principal tendo que ser chamada no final do código para que o test de erro funcione, porque se ela fosse chamada antes da exessao seria retornada uma erro sem ser a exessao;
 }
-getEmployeesCoverage();
+
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const horarios = Object.keys(data.hours);
+const sla = horarios.reduce((acc, curr) => {
+  const vi = acc + curr;
+  return vi;
+}, {});
+console.log(sla);
 
+function getSchedule(scheduleTarget) {
+}
+getSchedule();
+// const daysWeek = {
+//   horarios: {
+//     officeHour: `Open from ${horarios.Tuesday.open}am until ${horarios.Tuesday.close}pm`,
+//     exhibition: data.species.filter((element) => element.availability.includes('Tuesday'))
+//       .map((animal) => animal.name),
+//   },
+
+// };
 module.exports = getSchedule;
