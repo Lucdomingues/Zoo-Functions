d278a59b055f32244b731644ad91dfda6fd3a1ac
Lucdomingues <lojalucc@outlook.com>
2023-03-15 16:52:51 -0300

    finish

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,21 +1,10 @@
 const data = require('../data/zoo_data');
 
 const horarios = Object.keys(data.hours);
-const sla = horarios.reduce((acc, curr) => {
-  const vi = acc + curr;
-  return vi;
-}, {});
-console.log(sla);
+console.log(horarios);
 
 function getSchedule(scheduleTarget) {
 }
 getSchedule();
-// const daysWeek = {
-//   horarios: {
-//     officeHour: `Open from ${horarios.Tuesday.open}am until ${horarios.Tuesday.close}pm`,
-//     exhibition: data.species.filter((element) => element.availability.includes('Tuesday'))
-//       .map((animal) => animal.name),
-//   },
 
-// };
 module.exports = getSchedule;
